574924d9b2c62c21d8015bf77aea627cc6285c71
sierra182 <sierra-echo@msn.com>
2024-04-02 11:06:36 +0200

    Revert "test other sol"
    This reverts commit 898e767b9b94ebf5b1ca9c52aebf1bef7fb76e66.

--- sources/routine/routine.c
+++ sources/routine/routine.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 10:37:50 by seblin            #+#    #+#             */
-/*   Updated: 2024/04/02 10:56:17 by seblin           ###   ########.fr       */
+/*   Updated: 2024/04/01 19:46:09 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,16 +59,14 @@ static int	is_satiated(t_philo *philo)
 	}
 	return (is_satiated);
 }
-int	is_odd(t_philo *philo);
+
 void	*philo_routine(void *arg)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *) arg;
-	// while (!is_ready_to_start_dinning(philo))
-	// 	;
-	// if (!is_odd(philo))
-	// 	usleep(philo->data->eat_time / 2);
+	while (!is_ready_to_start_dinning(philo))
+		;
 	while (1)
 	{
 		if (is_end_needed(philo))
