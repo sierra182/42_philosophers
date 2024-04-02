5f6a8980588b481e8aafbea4cedd07d2f999d706
sierra182 <sierra-echo@msn.com>
2024-04-02 14:51:48 +0200

    no is ready

--- sources/routine/routine.c
+++ sources/routine/routine.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 10:37:50 by seblin            #+#    #+#             */
-/*   Updated: 2024/04/02 14:47:04 by seblin           ###   ########.fr       */
+/*   Updated: 2024/04/02 14:51:36 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,8 +65,8 @@ void	*philo_routine(void *arg)
 	t_philo	*philo;
 
 	philo = (t_philo *) arg;
-	while (!is_ready_to_start_dinning(philo))
-		;
+	// while (!is_ready_to_start_dinning(philo))
+	// 	;
 	while (1)
 	{
 		if (is_end_needed(philo))
