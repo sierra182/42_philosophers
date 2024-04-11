b143660d83423229451790545ed1c3d098c22b5b
sierra182 <sierra-echo@msn.com>
2024-04-11 08:10:47 +0200

    change return value is alone

--- philo_bonus/sources/main.c
+++ philo_bonus/sources/main.c
@@ -6,7 +6,7 @@
 /*   By: seblin <seblin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 11:37:55 by svidot            #+#    #+#             */
-/*   Updated: 2024/04/11 08:05:18 by seblin           ###   ########.fr       */
+/*   Updated: 2024/04/11 08:10:10 by seblin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	main(int argc, char *argv[])
 	if (setup(argv, &data, &philos))
 		return (flush_exit_struct(), 1);
 	if (is_alone(data))
-		return (flush_exit_struct(), 1);
+		return (flush_exit_struct(), 0);
 	if (make_childs(data, philos))
 		return (flush_exit_struct(), 1);
 	if (death_noticer(data, philos))
